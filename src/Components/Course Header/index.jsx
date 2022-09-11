f5270084dfe5fdb16f6f8407359ada626b104986
Ahmed Elsayed <ahm.abdelwahab@nu.edu.eg>
2022-09-11 12:02:26 +0200

    header styling init

--- src/Components/Course Header/index.jsx
+++ src/Components/Course Header/index.jsx
@@ -1,17 +1,36 @@
 import Rating from "@mui/material/Rating";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
 import exclamition from "../../Resources/SVGs/exclamation-mark.svg";
 import language from "../../Resources/SVGs/language.svg";
 import caption from "../../Resources/SVGs/captioning.svg";
 
 const CourseHeader = ({ selectedCourseSummary }) => {
-  console.log(selectedCourseSummary);
+  console.log("selectedCourseSummary: ", selectedCourseSummary);
 
   return (
-    <div id="courseHeaderContainer">
-      <div id="courseHeaderContent">
-        <div id="courseTitle">{selectedCourseSummary?.title}</div>
-        <div id="courseHeadline">{selectedCourseSummary?.headline}</div>
-        <div id="courseRatingContainer">
+    <div id="courseHeaderContainer" className="conatiner bg-lightBlack">
+      <div id="courseHeaderImg" className="w-4/6 mx-auto">
+        <img
+          className="pt-5"
+          src={selectedCourseSummary?.image_750x422}
+          alt={selectedCourseSummary?.title}
+        />
+      </div>
+      <div
+        id="courseHeaderContent"
+        className="w-4/6 mx-auto mt-4 font-ubuntu text-white"
+      >
+        <div id="courseTitle" className="text-4xl font-bold mb-3">
+          {selectedCourseSummary?.title}
+        </div>
+        <div id="courseHeadline" className="text-xl font-normal mb-3">
+          {selectedCourseSummary?.headline}
+        </div>
+        <div
+          id="courseRatingContainer"
+          className="flex text-4xl font-light mb-3"
+        >
           <span id="courseRating">
             {Math.round(selectedCourseSummary?.rating * 10) / 10}
           </span>
@@ -20,6 +39,9 @@ const CourseHeader = ({ selectedCourseSummary }) => {
               name="read-only"
               value={Math.round(selectedCourseSummary?.rating * 10) / 10}
               precision={0.5}
+              emptyIcon={
+                <StarBorderIcon style={{ opacity: 1 }} fontSize="inherit" />
+              }
               readOnly
             />
           </span>
@@ -49,7 +71,9 @@ const CourseHeader = ({ selectedCourseSummary }) => {
         <div id="lastUpdated">
           <img className="w-4 h-4" src={exclamition} alt="exclamation" />
           {`Last updated `}
-          {selectedCourseSummary?.last_update_date}
+          {`${Number(
+            selectedCourseSummary?.last_update_date?.split("-")[1]
+          )}/${Number(selectedCourseSummary?.last_update_date?.split("-")[0])}`}
         </div>
         <div id="courseLanguage">
           <img className="w-4 h-4" src={language} alt="language" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        black: "#000000",
-        white: "#ffffff",
-        darkGray: "#6a6f73",
-        lightGray: "#d1d7dc",
-        bronzeOlive: "#3d3c0a",
-        midYellow: "#eceb98",
-        lightOrange: "rgb(255, 166, 0)",
-        darkOrange: "#b4690e",
-      }
+    colors: {
+      black: "#000000",
+      lightBlack: "#1c1d1f",
+      white: "#ffffff",
+      darkGray: "#6a6f73",
+      lightGray: "#d1d7dc",
+      bronzeOlive: "#3d3c0a",
+      midYellow: "#eceb98",
+      lightOrange: "rgb(255, 166, 0)",
+      darkOrange: "#b4690e",
     },
+    fontFamily: {
+      lora: ["Lora", "serif"],
+      ubuntu: ["Ubuntu", "sans-serif"],
+      sans: ["sans-serif", "Arial", "Helvetica", "serif"],
+      rubik: ["Rubik", "sans-serif"],
+    }
   },
   plugins: [],
 }
