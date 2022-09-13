0299d386dccb9afcc1214560563fe60632f8b7f1
Ahmed Elsayed <ahm.abdelwahab@nu.edu.eg>
2022-09-13 22:50:01 +0200

    instructors section style

--- src/Components/Course Description/index.jsx
+++ src/Components/Course Description/index.jsx
@@ -12,8 +12,13 @@ const CourseDescription = ({
     );
     const showMoreBtn = document.getElementById("showMoreBtn");
     const showLessBtn = document.getElementById("showLessBtn");
-    if (courseDescription.classList.contains("h-80"))
+    if (
+      courseDescription.classList.contains("h-80") &&
+      courseDescription.classList.contains("overflow-hidden")
+    ) {
       courseDescription.classList.remove("h-80");
+      courseDescription.classList.remove("overflow-hidden");
+    }
     showMoreBtn.classList.toggle("hidden");
     showLessBtn.classList.toggle("hidden");
   };
@@ -25,8 +30,13 @@ const CourseDescription = ({
     );
     const showMoreBtn = document.getElementById("showMoreBtn");
     const showLessBtn = document.getElementById("showLessBtn");
-    if (!courseDescription.classList.contains("h-80"))
+    if (
+      !courseDescription.classList.contains("h-80") &&
+      !courseDescription.classList.contains("overflow-hidden")
+    ) {
       courseDescription.classList.add("h-80");
+      courseDescription.classList.add("overflow-hidden");
+    }
     showMoreBtn.classList.toggle("hidden");
     showLessBtn.classList.toggle("hidden");
   };
@@ -36,22 +46,22 @@ const CourseDescription = ({
       id="courseDescriptionContainer"
       className="w-11/12 max-w-3xl sm:w-4/5 lg:ml-20 xs:mx-auto mb-4"
     >
-      <div id="courseDescriptionContent" className="h-80 truncate">
+      <div id="courseDescriptionContent" className="h-80 overflow-hidden">
         <div id="courseDescriptionTxt" className="mb-4">
           <div id="courseDescriptionTitle" className="font-bold text-3xl mb-3">
             Description
           </div>
-          <pre className="overflow-x-auto leading-2">
+          <div className="overflow-x-auto leading-2">
             {selectedCourseDescription}
-          </pre>
+          </div>
         </div>
         <div id="forWho">
           <div id="forWhoTitle" className="font-bold text-3xl mb-3">
             Who this course is for:
           </div>
-          <pre id="forWhoTxt" className="leading-7 overflow-x-auto">
+          <div id="forWhoTxt" className="leading-7 overflow-x-auto">
             {selectedCourseForWho}
-          </pre>
+          </div>
         </div>
       </div>
       <button
--- src/Components/Course Instructors/index.jsx
+++ src/Components/Course Instructors/index.jsx
@@ -1,46 +1,102 @@
+import starIcon from "../../Resources/SVGs/star.svg";
+import certificationIcon from "../../Resources/SVGs/certificate.svg";
+import peopleIcon from "../../Resources/SVGs/people.svg";
+import playbtnIcon from "../../Resources/SVGs/play-button.svg";
+
 const CourseInstructors = ({ selectedCourseInstructors }) => {
   console.log("selectedCourseInstructors: ", selectedCourseInstructors);
+
+  /* function to show less about instructor info */
+  const showMore = (instructorInfoId, showMoreBtnId, showLessBtnId) => {
+    const instructorInfo = document.getElementById(instructorInfoId);
+    const showMoreBtn = document.getElementById(showMoreBtnId);
+    const showLessBtn = document.getElementById(showLessBtnId);
+    if (
+      instructorInfo.classList.contains("h-20") &&
+      instructorInfo.classList.contains("overflow-hidden")
+    ) {
+      instructorInfo.classList.remove("h-20");
+      instructorInfo.classList.remove("overflow-hidden");
+    }
+    showMoreBtn.classList.toggle("hidden");
+    showLessBtn.classList.toggle("hidden");
+  };
+
+  /* function to show less about instructor info */
+  const showLess = (instructorInfoId, showMoreBtnId, showLessBtnId) => {
+    const instructorInfo = document.getElementById(instructorInfoId);
+    const showMoreBtn = document.getElementById(showMoreBtnId);
+    const showLessBtn = document.getElementById(showLessBtnId);
+    if (
+      !instructorInfo.classList.contains("h-20") &&
+      !instructorInfo.classList.contains("overflow-hidden")
+    ) {
+      instructorInfo.classList.add("h-20");
+      instructorInfo.classList.add("overflow-hidden");
+    }
+    showMoreBtn.classList.toggle("hidden");
+    showLessBtn.classList.toggle("hidden");
+  };
+
   return (
-    <div id="courseInstructorsContainer">
+    <div
+      id="courseInstructorsContainer"
+      className="w-11/12 max-w-3xl sm:w-4/5 lg:ml-20 xs:mx-auto mb-4"
+    >
       <div id="courseInstructorsContent">
-        <div>Instructors</div>
+        <div className="font-bold text-3xl mb-3">Instructors</div>
         <div id="courseInstructorsInfo">
-          {selectedCourseInstructors.map((instructor, indx) => {
+          {selectedCourseInstructors?.map((instructor, indx) => {
             return (
-              <div id={`instructor${indx}`} key={instructor.id}>
-                <div id="instructorName">{instructor.title}</div>
-                <div id="instructorJobTitle">{instructor.job_title}</div>
-                <div id="instructorSummaryContainer">
-                  <div id="instructorImgContainer">
+              <div key={instructor.id} className="mb-4">
+                <div className="text-darkViolet font-bold underline">
+                  {instructor.title}
+                </div>
+                <div className="mb-2">{instructor.job_title}</div>
+                <div className="flex gap-x-2 items-center	mb-2">
+                  <div>
                     <img
+                      className="rounded-full mr-2"
                       src={instructor.image_100x100}
                       alt={instructor.title}
                     />
                   </div>
-                  <div id="instructorSummaryContent">
-                    <div id="instructorRating">
-                      <img src="" alt="" />
+                  <div>
+                    <div className="flex gap-x-2 items-center mb-2">
+                      <img src={starIcon} alt="star icon" className="w-4 h-4" />
                       <div>
                         {instructor.Instructor_Rating}
                         {` Instructor Rating`}
                       </div>
                     </div>
-                    <div id="instructorReviews">
-                      <img src="" alt="" />
+                    <div className="flex gap-x-2 items-center mb-2">
+                      <img
+                        src={certificationIcon}
+                        alt="certification icon"
+                        className="w-4 h-4"
+                      />
                       <div>
                         {instructor.Reviews}
                         {` Reviews`}
                       </div>
                     </div>
-                    <div id="instructorStudentsNum">
-                      <img src="" alt="" />
+                    <div className="flex gap-x-2 items-center mb-2">
+                      <img
+                        src={peopleIcon}
+                        alt="people icon"
+                        className="w-4 h-4"
+                      />
                       <div>
                         {instructor.Students}
                         {` Students`}
                       </div>
                     </div>
-                    <div id="instructorCoursesNum">
-                      <img src="" alt="" />
+                    <div className="flex gap-x-2 items-center">
+                      <img
+                        src={playbtnIcon}
+                        alt="play button icon"
+                        className="w-4 h-4"
+                      />
                       <div>
                         {instructor.Courses}
                         {` Courses`}
@@ -48,18 +104,37 @@ const CourseInstructors = ({ selectedCourseInstructors }) => {
                     </div>
                   </div>
                 </div>
-                <div id="instructorDescription">
-                  <div>{instructor.description}</div>
+                <div>
+                  <div
+                    id={`instructorInfo${indx}`}
+                    className="h-20 overflow-hidden"
+                  >
+                    {instructor.description}
+                  </div>
                 </div>
                 <button
-                  id="showMoreBtnInstructorDescription"
+                  id={`showMoreBtnInstructorInfo${indx}`}
                   className="text-darkViolet font-bold text-lg"
+                  onClick={() =>
+                    showMore(
+                      `instructorInfo${indx}`,
+                      `showMoreBtnInstructorInfo${indx}`,
+                      `showLessBtnInstructorInfo${indx}`
+                    )
+                  }
                 >
                   Show more
                 </button>
                 <button
-                  id="showLessBtnInstructorDescription"
+                  id={`showLessBtnInstructorInfo${indx}`}
                   className="hidden text-darkViolet font-bold text-lg"
+                  onClick={() =>
+                    showLess(
+                      `instructorInfo${indx}`,
+                      `showMoreBtnInstructorInfo${indx}`,
+                      `showLessBtnInstructorInfo${indx}`
+                    )
+                  }
                 >
                   Show less
                 </button>
